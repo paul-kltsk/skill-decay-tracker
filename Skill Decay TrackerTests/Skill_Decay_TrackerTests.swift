6c64c96b5c263f5f23f400255d761b56ca618d39
Pavel Kulitski <pshkltsk@gmail.com>
2026-03-16 15:17:04 +0300

    Initial Commit

---
+++ Skill Decay TrackerTests/Skill_Decay_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Skill_Decay_TrackerTests.swift
+//  Skill Decay TrackerTests
+//
+//  Created by Pavel Kulitski on 16.03.26.
+//
+
+import Testing
+@testable import Skill_Decay_Tracker
+
+struct Skill_Decay_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
